2dbae198decaf109a9599f49974cd1d4a44f29df
Qtzaguzman <jrguzman84@gmail.com>
2024-08-30 21:23:14 -0600

    Regresion PLS

--- Problema 1/a0_load_libraries.R
+++ Problema 1/a0_load_libraries.R
@@ -1,7 +1,41 @@
-library(gstat)
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(spdep)
-library(pls)
-library(caret)
+#Instalar librerias
+instalar = FALSE
+if(instalar) {
+    install.packages("ggplot2")
+    install.packages("dplyr")
+    install.packages("tidyr")
+    install.packages("spdep")
+    install.packages("pls")
+    install.packages("caret")
+    install.packages("raster")
+    install.packages("rgdal")
+    install.packages("rgeos")
+    install.packages("sp")
+    install.packages("zoo")
+    install.packages("fields")
+    install.packages("gstat", dependencies=TRUE)
+    install.packages("gridExtra")
+    install.packages("patchwork")
+}
+
+
+#Cargar librerias
+load = FALSE
+if(load) {
+    library(gstat)
+    library(ggplot2)
+    library(dplyr)
+    library(tidyr)
+    library(spdep)
+    library(pls)
+    library(caret)
+    library(raster)
+    library(rgdal)
+    library(rgeos)
+    library(sp)
+    library(zoo)
+    library(fields)
+    library(gridExtra)
+    library(patchwork)
+    library(caret)
+}
